6ca8b5d85763cb82ed7c4096dc1d3a77fa339cd9
Stephan Schumacher aka Worthy Alpaca <worthyalpaca@gmail.com>
2021-03-25 23:25:20 +0100

    cleaning code

--- src/events/message.js
+++ src/events/message.js
@@ -54,8 +54,6 @@ module.exports = client => {
                     })
                 }
             });
-
-
             
         } else {
             if (message.content.startsWith(prefix)) return runCommand(message);
