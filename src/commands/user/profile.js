839e069acdec8a79355b898e1b2c671f7c85d1ce
Stephan Schumacher aka Worthy Alpaca <worthyalpaca@gmail.com>
2021-03-26 10:31:43 +0100

    added TTS channel to profile

--- src/commands/user/profile.js
+++ src/commands/user/profile.js
@@ -9,16 +9,20 @@ module.exports = {
     execute: async (client, message, args) => {
         const embed = new Discord.MessageEmbed()
 
-        if (!db.has(`${message.author.id}.voice`)) {
+        if (!db.has(`${message.guild.id}${message.author.id}.voice`)) {
             embed.setColor('RED').setDescription(`❌ You have no voice registered. Use \`${config.prefix}register\` to register a new one!`);
             return message.reply(embed);
         }
-        const voice = message.guild.roles.cache.get(db.get(`${message.author.id}.voice`))
+        const voice = message.guild.roles.cache.get(db.get(`${message.guild.id}${message.author.id}.voice`))
         embed.setTimestamp();
         embed.setAuthor(`${message.member.displayName}`, message.member.user.displayAvatarURL());
-        embed.setDescription("Here you'll find all information stored about you");
+        embed.setDescription("Here you'll find all information needed to use TTS.");
         embed.addField('Voices', voice);
-        embed.setColor('RANDOM')
+        embed.setColor('RANDOM');
+        if (db.has(`${message.guild.id}.channel`)) {
+            const channel = message.guild.channels.cache.get(db.get(`${message.guild.id}.channel`));
+            embed.addField('Channel', channel);
+        }
         message.reply(embed);
         
     }
