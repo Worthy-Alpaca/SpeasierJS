65b3805c602b5548c2f13766a1a29c5feb4bd3cd
Stephan Schumacher aka Worthy Alpaca <worthyalpaca@gmail.com>
2021-04-07 15:00:13 +0200

    added command to view all available voices

---
+++ src/commands/info/voices.js
@@ -0,0 +1,22 @@
+const db = require('quick.db');
+const Discord = require('discord.js');
+const { voices } = require('../../assets/voices.json');
+module.exports = {
+	name: 'voices',
+	category: 'info',
+	description: 'Displays all available voices',
+	execute: async (client, message, args) => {
+
+		const cVoices = db.get(`${message.guild.id}.voices`) || voices.default;
+		const aVoices = voices.all.filter(n => !cVoices.includes(n));
+		const embed = new Discord.MessageEmbed()
+			.setTimestamp()
+			.setAuthor(message.guild.name, message.guild.iconURL())
+			.setTitle('Available Voices')
+			.addField('Current Voices', cVoices.join('\n'), true)
+			.addField('Available Voices', aVoices.join('\n'), true);
+		
+		return message.channel.send(embed);
+
+	}
+};
