5be80526ec6685ee80a8a4d07c21f10ed9ed0e61
Stephan Schumacher aka Worthy Alpaca <worthyalpaca@gmail.com>
2021-04-07 15:41:04 +0200

    added command to delete server config

--- src/commands/admin/delconfig.js
+++ src/commands/admin/delconfig.js
@@ -1,18 +1,19 @@
 const db = require('quick.db');
 module.exports = {
-	name: 'deletevoices',
+	name: 'deleteconfig',
 	category: 'admin',
-	description: 'Deletes the voice roles',
+	description: 'Deletes everything related to TTS',
 	execute: async (client, message, args) => {
+		if (!db.has(`${message.guild.id}`)) return message.reply('No config available!');
 		let voices = db.get(`${message.guild.id}.voices`);
-		if (!voices) return message.reply('No voices available!');
+		
 		voices.forEach(voice => {
 			let role = message.guild.roles.cache.find(r => r.name === voice);
 			if (!role) return;
 			role.delete();
 		});
-		db.delete(`${message.guild.id}.voices`);
-		message.reply('Roles successfully deleted');
+		db.delete(`${message.guild.id}`);
+		message.reply('Server successfully deleted from database.');
 
 	}
 };
