d5e0493ffd893840d50f3e777119a0d21d82bc56
Stephan Schumacher aka Worthy Alpaca <worthyalpaca@gmail.com>
2021-04-07 15:00:28 +0200

    added command to add voices

---
+++ src/commands/admin/addvoice.js
@@ -0,0 +1,34 @@
+const db = require('quick.db');
+const { voices } = require('../../assets/voices.json');
+module.exports = {
+	name: 'addvoice',
+	category: 'admin',
+	description: 'Adds a voice to be used and creates the corresponding role.',
+	execute: async (client, message, args) => {
+		
+		if (!db.has(`${message.guild.id}.voices`)) {
+			db.set(`${message.guild.id}.voices`, voices.default);
+		}
+
+		if (voices.all.includes(args[0])) {
+			if (db.get(`${message.guild.id}.voices`).includes(args[0])) {
+				return message.reply(`\`${args[0]}\` already exists.`);
+			}
+			db.push(`${message.guild.id}.voices`, args[0]);
+			let role = message.guild.roles.cache.find(r => r.name === args[0]);
+			if (!role) {
+				message.guild.roles.create({
+					data: {
+						name: args[0],
+						mentionable: true
+					},
+				})
+					.then(() => message.reply('Role created.'))
+					.catch((error) => message.reply(`Could not create a role for ${args[0]}`));
+			}
+		} else {
+			return message.reply(`Cannot create ${args[0]}! Please check if it's spelled correctly!`);
+		}
+
+	}
+};
