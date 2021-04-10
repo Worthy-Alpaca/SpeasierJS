28e99b07c7e9360ec60b63611d43388ce156b292
Stephan Schumacher aka Worthy Alpaca <worthyalpaca@gmail.com>
2021-04-11 00:10:58 +0200

    added command to make the bot leave

---
+++ src/commands/voice/leave.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+	name: 'leave',
+	category: 'voice',
+	description: 'Disconnects the bot',
+	execute: async (client, message, args) => {
+		const voiceChannel = message.member.voice.channel;
+		if (!voiceChannel) return message.reply('You need to be in a voice channel with the bot for this command to work!');
+		voiceChannel.leave().then(() => {
+			message.react('✔');
+		});
+	}
+};
