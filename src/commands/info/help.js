f3e5f41cea0c83d655f11c4109dd5568cfa0e1ce
Stephan Schumacher aka Worthy Alpaca <worthyalpaca@gmail.com>
2021-04-20 00:29:30 +0200

    fixed issue with specialized help not showing aliases

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -75,8 +75,8 @@ function getCMD(client, message, input) {
 	}
 
 	if (cmd.name) info = `**Command name**: ${cmd.name}`;
-	if (cmd.aliases) info = `\n**Aliases**: ${cmd.aliases.map(a => `\`${a}\``).join(', ')}`;
-	if (cmd.description) info = `\n**Description**: ${cmd.description}`;
+	if (cmd.aliases) info += `\n**Aliases**: ${cmd.aliases.map(a => `\`${a}\``).join(', ')}`;
+	if (cmd.description) info += `\n**Description**: ${cmd.description}`;
 	if (cmd.usage) {
 		info += `\n**Usage**: ${cmd.usage}`;
 		embed.setFooter('Syntax: <> = required, [] = optional');
