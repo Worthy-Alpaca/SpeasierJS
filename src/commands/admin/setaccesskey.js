a3c136fce36ee7ba1451b4a920e7e8b8661393cb
Stephan Schumacher aka Worthy Alpaca <worthyalpaca@gmail.com>
2021-06-25 15:27:57 +0200

    added source code link

--- src/commands/admin/setaccesskey.js
+++ src/commands/admin/setaccesskey.js
@@ -3,7 +3,7 @@ const { encrypt } = require('../../utils/functions');
 module.exports = {
 	name: 'setkeys',
 	category: 'admin',
-	description: 'Sets the access keys for AWS Polly. The message gets deleted immediatly and the keys encrypted.',
+	description: 'Sets the access keys for AWS Polly. The message gets deleted immediatly and the keys encrypted. You can view the source code [here](https://github.com/Worthy-Alpaca/SpeasierJS/blob/main/src/commands/admin/setaccesskey.js)',
 	usage: '<accessKeyID> <secretAccessKey>',
 	execute: async (client, message, args) => {
 		const accessKeyID = args[0];
