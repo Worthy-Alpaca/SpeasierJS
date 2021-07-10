c470aef041c89ee4fb8e2af2a0fe69cf4a6070f3
Stephan Schumacher aka Worthy Alpaca <worthyalpaca@gmail.com>
2021-07-10 21:07:24 +0200

    removed debug logs and added long description

--- src/commands/admin/setaccesskey.js
+++ src/commands/admin/setaccesskey.js
@@ -3,7 +3,8 @@ const { encrypt } = require('../../utils/functions');
 module.exports = {
 	name: 'setkeys',
 	category: 'admin',
-	description: 'Sets the access keys for AWS Polly. The message gets deleted immediatly and the keys encrypted. You can view the source code [here](https://github.com/Worthy-Alpaca/SpeasierJS/blob/main/src/commands/admin/setaccesskey.js)',
+	description: 'Sets the access keys for AWS Polly. The message gets deleted immediatly and the keys encrypted.',
+	descriptionlong: 'Sets the access keys for AWS Polly. The message gets deleted immediatly and the keys encrypted. You can view the source code [here](https://github.com/Worthy-Alpaca/SpeasierJS/blob/main/src/commands/admin/setaccesskey.js)',
 	usage: '<accessKeyID> <secretAccessKey>',
 	execute: async (client, message, args) => {
 		const accessKeyID = args[0];
@@ -15,11 +16,9 @@ module.exports = {
 		}
 
 		try {
-			console.log(accessKeyID, secretAccessKey);
 			db.set(`${message.guild.id}.config.accessKeyID`, encrypt(accessKeyID).toString());
 			db.set(`${message.guild.id}.config.secretAccessKey`, encrypt(secretAccessKey).toString());
 		} catch (error) {
-			console.error(error);
 			return message.reply(`An error occured: ${error}`);
 		}
 		return message.reply('Keys successfully encrypted.');
