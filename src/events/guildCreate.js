6fdf894c0c1907169b6cdb21454c5c4182fa53a3
Stephan Schumacher aka Worthy Alpaca <worthyalpaca@gmail.com>
2021-08-03 00:35:34 +0200

    added welcome message

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,9 +1,23 @@
-
+const Discord = require('discord.js');
 
 module.exports = client => {
 	client.on('guildCreate', guild => {
 		client.users.fetch('595341356432621573', false).then(user => {
 			user.send(`I was added to a new server: ${guild.name}, ${guild.id}`);
 		});
+
+		const embed = new Discord.MessageEmbed()
+			.setTimestamp()
+			.setTitle('Thank you for adding me!')
+			.setDescription('Thank you for adding me to your guild. Please see [here] for documentation on how to make me work for you.')
+			.setFooter(`Version: ${client.version}`);
+
+		let channel = guild.systemChannel();
+
+		if (!channel) {
+			return guild.owner.send(embed);
+		}
+
+		channel.send(embed);
 	});
 };
