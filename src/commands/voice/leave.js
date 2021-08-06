02c25a5cc8c853786b6f495946b4f5fdaa629cff
Stephan Schumacher aka Worthy Alpaca <worthyalpaca@gmail.com>
2021-08-07 01:13:04 +0200

    added method to disconnect the bot

--- src/commands/voice/leave.js
+++ src/commands/voice/leave.js
@@ -1,13 +1,16 @@
+const { getVoiceConnection } = require('@discordjs/voice');
 
 module.exports = {
 	name: 'leave',
 	category: 'voice',
 	description: 'Disconnects the bot',
 	execute: async (client, message, args) => {
-		const voiceChannel = message.member.voice.channel;
+		/* const voiceChannel = message.member.voice.channel;
 		if (!voiceChannel) return message.reply('You need to be in a voice channel with the bot for this command to work!');
 		message.react('✔').then(() => {
 			voiceChannel.leave();
-		});
+		}); */
+		const connection = getVoiceConnection(message.guild.id);
+		connection.destroy();
 	}
 };
